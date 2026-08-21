7081d5c554514cf9dc2f836776009e68b2f4f39d
HONYIEN <choo.honyien@gmail.com>
2026-08-21 15:49:21 +0800

    feat: Level-1, added echo functionality

--- src/main/java/Kelore.java
+++ src/main/java/Kelore.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Starts the Kelore chatbot application.
  */
@@ -9,13 +11,31 @@ public class Kelore {
                 + "| . \\ | |___| |___ | |_| |  _ <| |___\n"
                 + "|_|\\_\\|_____|_____| \\___/|_| \\_\\_____|";
         String divider = "_".repeat(60);
+        String indentation = "    ";
 
-        System.out.println(divider);
-        System.out.println(banner);
-        System.out.println("Hello! I'm Kelore.");
-        System.out.println("What can I do for you?");
-        System.out.println(divider);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(divider);
+        System.out.println(indentation + divider);
+        System.out.println(indentation + banner.replace("\n", "\n" + indentation));
+        System.out.println(indentation + "Hello! I'm Kelore.");
+        System.out.println(indentation + "What can I do for you?");
+        System.out.println(indentation + divider);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println(indentation + divider);
+
+            if ("bye".equals(input)) {
+                System.out.println(indentation + "Bye. Hope to see you again soon!");
+                System.out.println(indentation + divider);
+                break;
+            }
+
+            System.out.println(indentation + echoString(input));
+            System.out.println(indentation + divider);
+        }
+    }
+
+    public static String echoString(String input) {
+        return input;
     }
 }
