14559e1b9dee900bccfe0f31df52986faf623b26
HONYIEN <choo.honyien@gmail.com>
2026-08-21 15:21:25 +0800

    feat: Level-0, renamed to Kelore, added basic greetings

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Kelore User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Kelore.java
@@ -0,0 +1,21 @@
+/**
+ * Starts the Kelore chatbot application.
+ */
+public class Kelore {
+    public static void main(String[] args) {
+        String banner = " _  __ _____ _       ___  ____  _____\n"
+                + "| |/ /| ____| |     / _ \\|  _ \\| ____|\n"
+                + "| ' / |  _| | |    | | | | |_) |  _|\n"
+                + "| . \\ | |___| |___ | |_| |  _ <| |___\n"
+                + "|_|\\_\\|_____|_____| \\___/|_| \\_\\_____|";
+        String divider = "_".repeat(60);
+
+        System.out.println(divider);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Kelore.");
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(divider);
+    }
+}
